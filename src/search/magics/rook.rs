582085691efc439968098f420bd42abc3505d40f
Liam Blanton <145174304+LiamBlanton17@users.noreply.github.com>
2026-07-08 15:09:30 -0700

    wip: magics

--- src/search/magics/rook.rs
+++ src/search/magics/rook.rs
@@ -2,8 +2,11 @@ use crate::{search::move_generation::{MoveGenLevel, generate_ray_moves}, types::
 
 
 static mut ROOK_MASK: [BitBoard; 64] = [BitBoard(0); 64];
+static mut ROOK_MAGIC: [u64; 64] = [0; 64];
+static mut ROOK_BITS: [u32; 64] = [0; 64];
+static mut ROOK_ATTACK_TABLE: Vec<Vec<BitBoard>> = Vec::new();
 
-pub fn init_rook_mask() {
+pub fn init_rook_magic() {
 
     unsafe {
         for sq in 0..64 {
@@ -11,6 +14,41 @@ pub fn init_rook_mask() {
         }
     }
 
+}
+
+fn rook_attacks_slow(sq: Square, blockers: BitBoard) -> BitBoard {
+    let (sq_row, sq_col) = sq.to_row_col();
+    let mut attacks = BitBoard(0);
+
+    // North
+    for row in (sq_row + 1)..8 {
+        let target = Square::from_row_col(row, sq_col).to_bitboard();
+        attacks |= target;
+        if blockers & target != BitBoard(0) { break; }
+    }
+
+    // South
+    for row in (0..sq_row).rev() {
+        let target = Square::from_row_col(row, sq_col).to_bitboard();
+        attacks |= target;
+        if blockers & target != BitBoard(0) { break; }
+    }
+
+    // East
+    for col in (sq_col + 1)..8 {
+        let target = Square::from_row_col(sq_row, col).to_bitboard();
+        attacks |= target;
+        if blockers & target != BitBoard(0) { break; }
+    }
+
+    // West
+    for col in (0..sq_col).rev() {
+        let target = Square::from_row_col(sq_row, col).to_bitboard();
+        attacks |= target;
+        if blockers & target != BitBoard(0) { break; }
+    }
+
+    attacks
 }
 
 fn rook_mask_for_square(sq: Square) -> BitBoard {
