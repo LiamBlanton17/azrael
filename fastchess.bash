81c682219b31d9bcf7f515da0701be9c57e76f19
Liam Blanton <145174304+LiamBlanton17@users.noreply.github.com>
2026-07-29 11:54:56 -0700

    chore: update fastchess bash file

--- fastchess.bash
+++ fastchess.bash
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 ./fastchess/fastchess \
-    -engine cmd=./azrael_feature name=feature \
-    -engine cmd=./azrael_main    name=main \
+    -engine cmd=./azrael_feature.exe name=feature \
+    -engine cmd=./azrael_main.exe    name=main \
     -each proto=uci tc=30+1 \
     -openings file=./openings.epd format=epd order=random \
     -rounds 40000 -games 2 -repeat \
@@ -13,7 +13,5 @@ set -euo pipefail
     -draw movenumber=40 movecount=8 score=10 \
     -recover \
     -ratinginterval 250 -report penta=true \
-    -config file=config.json \
     -pgnout file=out.pgn \
     -log file=fc.log level=warn
-    
